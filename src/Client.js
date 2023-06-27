637f90fff2a27aac09b69baec99470e5a79e413b
Hadwork Indonesia <dev.hadworkid@gmail.com>
2023-06-28 01:43:28 +0700

    add: onMessage method

--- src/Client.js
+++ src/Client.js
@@ -1,10 +1,14 @@
 const { parsePhoneNumber } = require("awesome-phonenumber");
 const Validator = require("./validator");
+const { serialize } = require("./lib/serialize");
 
 class Client {
   constructor(client, sessionName) {
     this.client = client;
     this.sessionName = sessionName;
+    this.onMessageCallback = null;
+
+    this.onMessageUpsert();
   }
 
   async sendMessage({ to, type, content }) {
@@ -20,6 +24,40 @@ class Client {
       content,
     );
     console.log(`Send message to : ${to}`);
+  }
+
+  async onMessageUpsert() {
+    this.client.ev.on('messages.upsert', (m) => {
+      console.log(m);
+      if(this.onMessageCallback) {
+        this.onMessageCallback(m);
+      }
+    });
+  }
+
+  async onMessage(cb) {
+    this.onMessageCallback = (m) => {
+      if (!m.messages) return;
+
+      let msg = m.messages[0];
+      if (msg.status) return;
+      if (msg.broadcast) return;
+
+      msg = serialize(this.client, m.messages[0]);
+      const senderId = msg.key.remoteJid;
+      const senderNumber = senderId.replace(/\D/g, '').replace('@s.whatsapp.net', '');
+
+      const params = {
+        sender: {
+          id: msg.key.remoteJid,
+          number: senderNumber,
+        },
+        content: {
+          text: msg.text,
+        }
+      }
+      cb(params);
+    }
   }
 }
 
